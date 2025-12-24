4a8db0d04ab183b27b3a47d3066c81b342cf4769
Avash Mahato <mahatoavash90@gmail.com>
2025-12-24 08:06:23 +0545

    payment routes

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,22 +1,49 @@
+// import { Router } from "express";
+// import { authSTD } from "../middlewares/stdAuth.middleware.js";
+// import { coursePayment, coursePaymentConfirmation, getkey, teacherAmount, withdrawAmount } from "../controllers/payment.controller.js";
+// import { authTeacher } from "../middlewares/teacherAuth.middleware.js";
+
+
+// const router = Router()
+
+// router.route("/course/:courseID/:coursename").post(authSTD, coursePayment)
+
+// router.route("/razorkey").get(authSTD, getkey)
+
+// router.route("/confirmation/course/:courseID").post(authSTD, coursePaymentConfirmation)
+
+// router.route("/teacher/:teacherID/balance").post(authTeacher, teacherAmount)
+
+// router.route("/teacher/:teacherID/withdraw").post(authTeacher, withdrawAmount)
+
+// export default router;
+
 import { Router } from "express";
 import { authSTD } from "../middlewares/stdAuth.middleware.js";
-import { coursePayment, coursePaymentConfirmation, getkey, teacherAmount, withdrawAmount } from "../controllers/payment.controller.js";
+import {
+  coursePayment,
+  khaltiCallback,
+  coursePaymentConfirmation,
+  getkey,
+  teacherAmount,
+  withdrawAmount,
+} from "../controllers/payment.controller.js";
 import { authTeacher } from "../middlewares/teacherAuth.middleware.js";
 
+const router = Router();
 
-const router = Router()
+router.route("/course/:courseID/:coursename").post(authSTD, coursePayment);
 
-router.route("/course/:courseID/:coursename").post(authSTD, coursePayment)
+router.route("/khalti-key").get(authSTD, getkey);
 
-router.route("/razorkey").get(authSTD, getkey)
+router.route("/khalti-callback").get(khaltiCallback);
 
-router.route("/confirmation/course/:courseID").post(authSTD, coursePaymentConfirmation)
+router
+  .route("/confirmation/course/:courseID")
+  .post(authSTD, coursePaymentConfirmation);
 
-router.route("/teacher/:teacherID/balance").post(authTeacher, teacherAmount)
+router.route("/teacher/:teacherID/balance").post(authTeacher, teacherAmount);
 
-router.route("/teacher/:teacherID/withdraw").post(authTeacher, withdrawAmount)
-
-
-
+router.route("/teacher/:teacherID/withdraw").post(authTeacher, withdrawAmount);
 
 export default router;
